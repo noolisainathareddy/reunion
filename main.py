6ceb5bdda259f3cf4473a75f987dce4a710b07a8
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2026-02-17 15:27:39 -0600

    updated post method

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from socket import fromfd
+
 import fastapi
 import pydantic
-
+from typing import Optional
+from random import randrange
 
 app  = fastapi.FastAPI()
 
 class Post(pydantic.BaseModel):
     name : str
     place : str
-    zipcode: int
+    zipcode: Optional[int] = None
 
+my_posts = [{"name" : "sai", "place"  :  "vemcity", "zipcode" : 516349}, {"name" : "Hema", "place"  :  "PLVD", "zipcode" : 516329}]
 
 @app.get("/health")
 def healthCheck():
@@ -17,9 +21,11 @@ def healthCheck():
 
 @app.post("/upload")
 def postContent(new_post : Post):
-    print(type(new_post))
-    print(new_post.model_dump_json())
-    return {"Posted Data Successfully"}
+    content = new_post.model_dump()
+    print(type(content))
+    content.update({ "zipcode": randrange(0,10000 )})
+    return {"message" : "Posted Data Successfully", "data" : content}
+
 
 
 
